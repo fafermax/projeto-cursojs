bc36e66ead9c38354852b399856da275ef9ca29d
Fabio Maximiliano Ferro <fafermaxx@gmail.com>
2023-03-08 16:01:23 -0400

    atualizacao alert

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,3 +1,11 @@
 function verificar(){
-    window.alert(`Funcionou`)
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById(`txtano`)
+   var res = document.querySelector(`div#res`)
+   if (fano.ariaValueMax.length ==0 || fano.value > ano) {
+    window.alert(`Verifique os dados e tente novamente`)
+   }else {
+    window.alert(`Tudo certo!`)
+   }
 }
