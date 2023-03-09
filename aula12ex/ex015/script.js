404255373d2176f9d5e6eaca763afd4f0ed1c93b
Fabio Maximiliano Ferro <fafermaxx@gmail.com>
2023-03-09 12:49:54 -0400

    atualizei o codigo

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,11 +1,11 @@
-function verificar(){
+function verificar() {
    var data = new Date()
    var ano = data.getFullYear()
    var fano = document.getElementById(`txtano`)
    var res = document.querySelector(`div#res`)
-   if (fano.ariaValueMax.length ==0 || fano.value > ano) {
+   if (fano.value == 0 || fano.value > ano) {
     window.alert(`Verifique os dados e tente novamente`)
    }else {
-    window.alert(`Tudo certo!`)
+    var fsex = 
    }
 }
