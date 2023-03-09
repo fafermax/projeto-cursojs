5cfcf315f14ea4704a0e1f9a5bddb27a7a733a2b
Fabio Maximiliano Ferro <fafermaxx@gmail.com>
2023-03-09 13:50:34 -0400

    adicionei as funcoes para fotos

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,11 +3,50 @@ function verificar() {
    var ano = data.getFullYear()
    var fano = document.getElementById(`txtano`)
    var res = document.querySelector(`div#res`)
+
    if (fano.value == 0 || fano.value > ano) {
     window.alert(`Verifique os dados e tente novamente`)
-   }else {
+   } else {
     var fsex = document.getElementsByName(`radsex`)
     var idade = ano - Number(fano.value)
-    res.innerHTML = `Idade Calculada: ${idade}`
+    //res.innerHTML = `Idade Calculada: ${idade}`... so para testar se estava correto.
+    var genero = ``
+    //criacao da tag de imagem
+    var img = document.createElement(`img`)
+    img.setAttribute(`id`,`foto`)
+    if (fsex[0].checked) {
+      genero = `Homem`
+      if (idade >=0 && idade <10) {
+         //crianca
+         img.setAttribute(`src`,`bebe_homem.jpg`)
+      } else if (idade < 21) {
+         //jovem
+         img.setAttribute(`src`, `jovem_homem.jpg`)
+      } else if (idade < 50) {
+         //adulto
+         img.setAttribute(`src`,`adulto_homem.jpg`)
+      } else {
+         //idoso
+         img.setAttribute(`src`,`idoso_homem.jpg`)
+      }
+    } else if (fsex[1].checked) {
+       genero = `Mulher`
+       if (idade >=0 && idade < 10) {
+         //crianca
+         img.setAttribute(`src`,`bebe_mulher.jpg`)
+      } else if (idade < 21) {
+         //jovem
+         img.setAttribute(`src`,`jovem_mulher.jpg`)
+      } else if (idade < 50) {
+         //adulto
+         img.setAttribute(`src`,`adulto_mulher.jpg`)
+      } else {
+         //idoso
+         img.setAttribute(`src`,`idoso_mulher.jpg`)
+      }
+   }
+    res.style.textAlign = `center`
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+    res.appendChild(img)
    }
 }
