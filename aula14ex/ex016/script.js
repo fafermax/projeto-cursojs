92715166ebf03bfc02b329d1f195750b26bba326
Fabio Maximiliano Ferro <fafermaxx@gmail.com>
2023-03-10 10:41:16 -0400

    atualizei versao

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,23 +1,11 @@
-function carregar() {
+function contar() {
+    let ini = document.getElementById(`txti`)
+    let fim = document.getElementById(`txtf`)
+    let passo = document.getElementById(`txtp`)
 
-    var msg = document.getElementById(`msg`)
-    var img = document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-   // teste sem mudar o relogio do windows var hora = 23
-    msg.innerHTML = `Agora sao ${hora} horas.`
-    if (hora >=0 && hora <12) {
-        //bom dia
-        img.src = `manha.jpg`
-        document.body.style.background = `#e2cd9f`
-    } else if (hora >= 12 && hora < 18) {
-        //boa tarde
-        img.src = `tarde.jpg`
-        document.body.style.background = `#b9846f`
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0) {
+        window.alert(`[ERRO] faltam dados!`)
     } else {
-        //boa noite
-        img.src = `noite.jpg`
-        document.body.style.background = `#515154`
+        alert(`TUDO CERTO`)
     }
-
 }
