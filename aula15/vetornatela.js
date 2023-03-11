d7dabc9e685ee04a44e999ae622fcec94a21ff07
Fabio Maximiliano Ferro <fafermaxx@gmail.com>
2023-03-10 21:56:27 -0400

    funcoes js

--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -3,3 +3,10 @@ let valores = [8, 1, 7, 4, 2, 9]
 for(let pos=0; pos < valores.length; pos++) {
     console.log(`A posicao ${pos} tem o valor ${valores[pos]}`)
 }
+
+ou
+
+for(let pos in num) {
+    console.log(`A posicao ${pos} tem o valor ${valores[pos]}`)
+ 
+}
---
+++ aula16/funcao01.js
@@ -0,0 +1,9 @@
+function parimpar(){
+    if(n%2 == 0) {
+        return `Par`
+    } else {
+        return `impar`
+    }
+}
+let res = parimpar(4)
+console.log(res)
